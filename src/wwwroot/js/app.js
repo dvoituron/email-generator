81348128e7af4297678622fac0f52f938587f5e7
Denis Voituron <denis@voituron.net>
2021-06-28 18:28:47 +0200

    Add copy to clipboard feature

---
+++ src/wwwroot/js/app.js
@@ -0,0 +1,10 @@
+﻿CopyToClipboard = function (id) {    
+    var r = document.createRange();
+    r.selectNode(document.getElementById(id));
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(r);
+    document.execCommand('copy');
+    window.getSelection().removeAllRanges();
+    alert("Signature copiée.");
+    return true;
+}
